fb032725042a73556bab8687682014e3074f60e7
thivaanjchani <136246336+thivaanjchani@users.noreply.github.com>
2023-06-28 20:21:45 +0530

    Add files via upload

--- robot_builder.py
+++ robot_builder.py
@@ -1,3 +1,18 @@
+#eyes#
+t.goto(-60,160)
+rectangle(30,10,'white')
+t.goto(-60,160)
+rectangle(5,5,'black')
+t.goto(-45,155)
+rectangle(5,5,'black')
+
+#mouth
+t.goto(-65,135)
+t.right(5)
+rectangle(40,5,'black')
+
+t.hideturtle()
+
 import turtle as t
 def rectangle(horizontal,vertical,color):
     t.pendown()
